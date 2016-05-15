ea898f8434e8100d7fbdf73f4bc7f02a15dc20c2
mohammad ali <ma.nasrollahi71@yahoo.com>
2016-05-15 13:37:55 +0430

    master page & article

--- edu_site/__init__.py
+++ edu_site/__init__.py
@@ -8,6 +8,7 @@ def main(global_config, **settings):
     config.include('pyramid_chameleon')
     config.add_static_view('static', path='edu_site:', cache_max_age=3600)
     config.add_route('home', '/')
-    config.add_route('#', '/')
+    config.add_route('#', '')
+    config.add_route('artc', '/{artc_name}')
     config.scan('.views')
     return config.make_wsgi_app()
--- edu_site/views.py
+++ edu_site/views.py
@@ -1,13 +1,32 @@
+from pyramid.events import subscriber
+from pyramid.interfaces import IBeforeRender
+from pyramid.renderers import get_renderer
 from pyramid.view import (
     view_config,
     view_defaults)
 
 
 @view_defaults(renderer='templates/index.pt')
-class TutorialViews:
+class Views:
     def __init__(self, request):
         self.request = request
 
     @view_config(route_name='home')
-    def my_view(request):
+    def index(request):
         return {'name': 'edu site'}
+
+    @view_config(route_name='artc' , renderer='templates/article.pt')
+    def article(self):
+        arct_name=self.request.matchdict['artc_name']
+        return {'pagetitle': arct_name}
+
+    # @view_config(route_name='#')
+    # def artc(self):
+    #     arct_name=self.request.matchdict['artc_name']
+    #     return arct_name
+
+
+@subscriber(IBeforeRender)
+def globals_factory(event):
+    master = get_renderer('templates/master.pt').implementation()
+    event['master'] = master
